9cda3127b070bf874a93cf565543d5ba85d587e7
screwyterror83 <screwyterror83@gmail.com>
2020-04-23 23:51:08 -0700

    Wiki API basic functionality completed

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const _ = require("lodash");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static("public"));
 
@@ -15,8 +17,7 @@ const dbName = "wikiDB";
 
 mongoose.connect(
   "mongodb+srv://admin-darren:Mongodb382%23@cluster0-gmncq.mongodb.net/" +
-    dbName,
-  {
+  dbName, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useFindAndModify: false,
@@ -30,19 +31,104 @@ const articleSchema = new mongoose.Schema({
 
 const Article = mongoose.model("Article", articleSchema)
 
+// TO DO
 
+/* using a express module method "app.route()" to chain all the http request to 
+the same route together, details in https://expressjs.com/en/guide/routing.html */
 
+app.route('/articles')
 
+  .get((req, res) => {
 
+    Article.find({}, (err, foundArticles) => {
+      if (!err) {
+        res.send(foundArticles)
+      } else {
+        res.send(err)
+      }
+    })
+  })
 
+  .post((req, res) => {
+    // console.log(req.body.title);
+    // console.log(req.body.content);
 
+    const newArticle = new Article({
+      title: req.body.title,
+      content: req.body.content,
+    });
 
+    newArticle.save((err) => {
+      if (err) {
+        res.send(err)
+      } else {
+        res.send("Successfully added new article.");
+      }
+    })
+  })
 
+  .delete((req, res) => {
+    Article.deleteMany({}, (err) => {
+      if (err) {
+        res.send(err)
+      } else {
+        res.send("Successfully deleted all articles.")
+      }
+    })
+  });
 
+app
+  .route("/articles/:articleTitle")
+  .get((req, res) => {
+    Article.findOne({ title: req.params.articleTitle }, (err, article) => {
+      if (!article) {
+        res.send("No Article by the name: " + req.params.articleTitle + " was found.");
+      } else {
+        res.send(article)
+      }
+    });
+  })
+  .put((req, res) => {
+    // Article.update(
+    //   { title: req.params.articleTitle },
+    //   { title: req.body.title, content: req.body.content },
+    //   { overwrite: true }, (err) => {
+    //     if (!err) {
+    //       res.send("Successfully updated article")
+    //     }
+    //   })
+  
 
+    Article.findOneAndUpdate({ title: req.params.articleTitle }, { title: req.body.title, content: req.body.content }, { upsert: true, }, (err) => {
+      if (!err) {
+        res.send("Article has been successfully updated")
+      }
+    })
+  })
 
+  .patch((req, res) => {
 
-
+      Article.findOneAndUpdate(
+        { title: req.params.articleTitle },
+        { $set: req.body },
+        // { upsert: true },
+        /* patch doesn't need to create new doc, it's merely update field of document */
+        (err) => {
+          if (!err) {
+            res.send("Article has been successfully updated");
+          } else {
+            res.send(err);
+          }
+        }
+      );
+  })
+  .delete((req, res) => {
+    Article.deleteOne({ title: req.params.articleTitle }, (err) => {
+      if (!err) {
+        res.send("Corresponding article has been successfully deleted")
+      }
+    })
+  });
 
 let port = process.env.PORT
 if (port == null || port == "") {
